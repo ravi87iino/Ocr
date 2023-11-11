0a5998b5849d8183927888fc00856aa43b2bd36c
ravi87iino <89132761+ravi87iino@users.noreply.github.com>
2023-11-11 16:29:24 +0530

    Update ocrDetect.py

--- ocrDetect.py
+++ ocrDetect.py
@@ -1,11 +1,11 @@
-import cv2
-import pytesseract
+import cv2 
+import easyocr
 import streamlit as st
 import numpy as np
 from PIL import Image
 
 def main():
-    st.title("Pytesseract Text Detection")
+    st.title("EasyOCR Text Detection")
     img = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
 
     if img is not None:
@@ -13,36 +13,30 @@ def main():
         st.image(image, caption='Uploaded Image.', use_column_width=True)
         st.write("")
 
-        # Convert the image to grayscale
-        gray_img = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2GRAY)
+        reader = easyocr.Reader(['en'])
+        result = reader.readtext(np.array(image))
 
-        # Draw green boxes around detected text
         final_img = np.array(image)
-        h, w = gray_img.shape
-        boxes = pytesseract.image_to_boxes(gray_img)
-        for b in boxes.splitlines():
-            b = b.split()
-            top_left = (int(b[1]), h - int(b[2]))
-            bottom_right = (int(b[3]), h - int(b[4]))
-            text = b[0]
+        for detection in result:
+            top_left = tuple(map(int, detection[0][0]))
+            bottom_right = tuple(map(int, detection[0][2]))
+            text = detection[1]
             final_img = cv2.rectangle(final_img, top_left, bottom_right, (0, 255, 0), 3)
-            final_img = cv2.putText(final_img, text, (top_left[0], top_left[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+            final_img = cv2.putText(final_img, text, top_left, cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
 
-        # Display the image with green boxes
         st.image(final_img, caption='Result Image.', use_column_width=True)
 
-        # Use pytesseract to extract words
-        result = pytesseract.image_to_string(gray_img)
-        res = [result]
+        df = [detection[1] for detection in result]
         st.write("Detected Text:")
-        st.write(res)
-        # Download Button for Text
+        st.write(df)
+
+        # Download Button
         download_button = st.download_button(
             label="Download Detected Text",
-            data=result,
+            data='\n'.join(df),
             file_name='detected_text.txt',
             mime='text/plain'
         )
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     main()
